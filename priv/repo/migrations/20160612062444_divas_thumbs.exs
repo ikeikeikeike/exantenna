5ed12506bcd7f0dab2dea81b616789ec32bd051b
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-06-12 16:34:32 +0900

    diva thumb table

---
+++ priv/repo/migrations/20160612062444_divas_thumbs.exs
@@ -0,0 +1,25 @@
+defmodule Exantenna.Repo.Migrations.DivasThumbs do
+  use Ecto.Migration
+
+  def change do
+    create table(:divas_thumbs) do
+      add :assoc_id, :integer
+
+      add :name, :string
+      add :src, :text
+      add :ext, :string
+      add :mime, :string
+      add :width, :integer
+      add :height, :integer
+
+      timestamps
+    end
+    create index(:divas_thumbs, [:assoc_id])
+
+    create index(:divas_thumbs, [:src])
+    create index(:divas_thumbs, [:ext])
+    create index(:divas_thumbs, [:width])
+    create index(:divas_thumbs, [:height])
+
+  end
+end
