ddf5b5c256b0dd183fa1d62338df93cc9489146b
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-06-14 22:45:14 +0900

    added chracter

---
+++ web/models/character.ex
@@ -0,0 +1,32 @@
+defmodule Exantenna.Character do
+  use Exantenna.Web, :model
+
+  schema "characters" do
+    field :name, :string
+    field :alias, :string
+    field :kana, :string
+    field :romaji, :string
+    field :gyou, :string
+
+    field :height, :integer
+    field :width, :integer
+
+    field :bust, :integer
+    field :bracup, :string
+    field :waist, :integer
+    field :hip, :integer
+
+    field :blood, :string
+    field :birthday, Ecto.Date
+
+    timestamps
+  end
+
+  @required_fields ~w(name)
+  @optional_fields ~w(alias kana romaji gyou height width bust bracup waist hip blood birthday)
+
+  def changeset(model, params \\ :invalid) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+end
