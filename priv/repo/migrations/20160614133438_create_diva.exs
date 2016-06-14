26a67c95e5670d8b9256553e96fec5d6e6d26db0
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-06-14 22:45:56 +0900

    added diva

---
+++ priv/repo/migrations/20160614133438_create_diva.exs
@@ -0,0 +1,37 @@
+defmodule Exantenna.Repo.Migrations.CreateDiva do
+  use Ecto.Migration
+
+  def change do
+    create table(:divas) do
+      add :name, :string
+      add :alias, :string
+      add :kana, :string
+      add :romaji, :string
+      add :gyou, :string
+
+      add :height, :integer
+      add :width, :integer
+
+      add :bust, :integer
+      add :bracup, :string
+      add :waist, :integer
+      add :hip, :integer
+
+      add :blood, :string
+      add :birthday, :date
+
+      timestamps
+    end
+    create index(:divas, [:name])
+    create index(:divas, [:kana])
+    create index(:divas, [:gyou])
+    create index(:divas, [:bust])
+    create index(:divas, [:hip])
+    create index(:divas, [:bracup])
+    create index(:divas, [:blood])
+    create index(:divas, [:height])
+    create index(:divas, [:weight])
+    create index(:divas, [:birthday])
+
+  end
+end
