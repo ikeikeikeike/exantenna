d60d094b7da7009ec26a1eb2e72d0ebaccf0df59
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-06-29 19:08:44 +0900

    some fix

--- lib/exantenna/services/antenna.ex
+++ lib/exantenna/services/antenna.ex
@@ -5,7 +5,10 @@ defmodule Exantenna.Services.Antenna do
   require Logger
 
   def update_by_feed(feed) do
-
+    Enum.each feed, fn f ->
+      video = Redis.video_get f.url
+      picture = Redis.picture_get f.url
+    end
   end
 
 end
