ec294f9c2ea0459db02b4dcb8161532cf626d812
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-07-06 18:24:07 +0900

    convinient query

--- web/models/antenna.ex
+++ web/models/antenna.ex
@@ -2,10 +2,9 @@ defmodule Exantenna.Antenna do
   use Exantenna.Web, :model
 
   schema "antennas" do
-    belongs_to :metadata, Exantenna.Metadata
-
     belongs_to :blog, Exantenna.Blog
     belongs_to :entry, Exantenna.Entry
+    belongs_to :metadata, Exantenna.Metadata
     belongs_to :video, Exantenna.Video
     belongs_to :picture, Exantenna.Picture
     belongs_to :summary, Exantenna.Summary
@@ -18,6 +17,31 @@ defmodule Exantenna.Antenna do
 
   @required_fields ~w()
   @optional_fields ~w()
+  @relational_fields ~w(blog entry metadata video picture summary penalty tags)a
+
+  def query do
+    from e in __MODULE__,
+     select: e,
+    preload: ^@relational_fields
+  end
+
+  def query_full do
+    from e in __MODULE__,
+    preload: [
+      :metadata,
+      :penalty,
+      blog: [:thumb, :penalty, :scores, :verifiers],
+      entry: [:thumbs, :scores],
+      video: :metadatas,
+      picture: :thumbs,
+      summary: :scores,
+      tags: :thumb,
+    ]
+  end
+
+  def query_pictures, do: query_full
+  def query_entries, do: query_full
+  def query_videos, do: query_full
 
   def changeset(model, params \\ :invalid) do
     model
