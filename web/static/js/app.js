0a86190068c14dc71fbcba6ab49a654ff49813a7
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-07-19 03:19:59 +0900

    design

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -20,31 +20,5 @@ import "phoenix_html"
 
 // import socket from "./socket"
 
-
-
-$(function() {
-
-    var $modal = $('.js-autocomplete-modal');
-
-    $modal.on('shown.bs.modal', function(e) {
-      var value = $('input.js-autocomplete-firedom').val(),
-          input = $(this).find('input[name="q"]');
-
-      input.val(value);
-      if ( !('createTouch' in document) && !('ontouchstart' in document)) {
-        input.focus();
-      }
-    });
-
-    $modal.on('hidden.bs.modal', function() {
-      var value = $(this).find('input[name="q"]').val(),
-          input = $('input.js-autocomplete-firedom');
-
-      input.val(value);
-      if ( !('createTouch' in document) && !('ontouchstart' in document)) {
-        setTimeout(function() { input.blur(); }, 1000);
-      }
-
-    });
-
-});
+import typeahead from "./header"
+import sidebar from "./sidebar"
