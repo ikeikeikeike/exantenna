7725bbefdf4b2344bae2b8e7064adf59ecc60180
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-09-02 14:27:12 +0900

    Added TODO

---
+++ lib/exantenna/builders/penalty.ex
@@ -0,0 +1,29 @@
+defmodule Exantenna.Builders.Penalty do
+  alias Exantenna.Repo
+  alias Exantenna.Blog
+  alias Exantenna.Score
+  alias Exantenna.Antenna
+  alias Exantenna.Penalty
+
+  import Ecto.Query, only: [from: 1, from: 2]
+  require Logger
+
+  # TODO: must make below particularly :begin status.
+
+  def penalty(:up) do
+
+  end
+
+  def penalty(:down) do
+
+  end
+
+  def penalty(:left) do
+
+  end
+
+  def penalty(:begin) do
+
+  end
+
+end
