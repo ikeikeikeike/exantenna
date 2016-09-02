8c426641aceaf28c5ce43d74789075d105452ca7
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-09-02 11:32:43 +0900

    fixed to correct display into next,prev link on show page using dealing each site logic.

--- web/controllers/blog_controller.ex
+++ web/controllers/blog_controller.ex
@@ -1,17 +1,32 @@
 defmodule Exantenna.BlogController do
   use Exantenna.Web, :controller
 
-  alias Exantenna.Antenna
   alias Exantenna.Blog
+  alias Exantenna.Video
+  alias Exantenna.Picture
+  alias Exantenna.Antenna
+
+  alias Exantenna.Filter
   alias Exantenna.Es.Paginator
 
   import Ecto.Query
 
   def show(conn, %{"id" => id} = params) do
     blog = Repo.get_by!(Blog.query_all(1), id: id)
+    qs = Antenna.query_all(:index)
+
+    queryable =
+      case Exantenna.Domain.Filter.what(conn) do
+        "video" ->
+          from q in qs, join: c in Video, where: c.elements >= ^Filter.allow_video_elements
+        "book"  ->
+          from q in qs, join: c in Picture, where: c.elements >= ^Filter.allow_picture_elements
+        _       ->
+          from q in qs
+      end
 
     pager =
-      Antenna.query_all
+      queryable
       |> where([q], q.blog_id == ^id)
       |> order_by([q], [desc: q.id])
       |> Repo.paginate(params)
